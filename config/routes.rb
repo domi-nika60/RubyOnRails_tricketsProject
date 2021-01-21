45b715542670c7471a4d3782c6fc66ad2c756db7
Dominika Furgala <dominikafurgala1@gmail.com>
2021-01-21 20:22:26 +0100

    Boostrap and devise gem usage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # devise_for :users
   get '/events'  => 'events#index', as: "events"
   get 'events/new'
   post '/events' => 'events#create'
   get 'events/:id' => 'events#show'
   resources :tickets
+  root :to => "events#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
